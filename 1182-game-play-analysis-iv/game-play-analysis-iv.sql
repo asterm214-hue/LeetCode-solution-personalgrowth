6c786b67d93e08b1f9b5346bfaf400aaa9fcb517
asterm214-hue <asterm214@gmail.com>
2026-08-13 21:36:37 +0530

    Time: 646 ms (35.21%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,11 +1,12 @@
 SELECT ROUND(
-    COUNT(DISTINCT player_id) /
-    (SELECT COUNT(DISTINCT player_id) FROM Activity),
+    COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity),
     2
 ) AS fraction
-FROM Activity
-WHERE (player_id, event_date) IN (
-    SELECT player_id, DATE_ADD(MIN(event_date), INTERVAL 1 DAY)
+FROM Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
-);
+) b
+ON a.player_id = b.player_id
+AND a.event_date = DATE_ADD(b.first_date, INTERVAL 1 DAY);
